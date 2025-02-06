9897a0b69154ea47e039352dcd86447af1f890f3
HUGO LOPES DE SOUZA <24122289@CFP127ALUNO.LOCAL>
2025-02-06 12:27:21 -0300

    produtos em andamento

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 'use strict'
 
 const disciplinas = [
-    {nome:'PPDM', icon: 'instagram.png'},
-    {nome:'PRO', icon: 'whatsapp.png'},
-    {nome:'PWBE', icon:'youtube.png'},
-    {nome:'PWFE', icon: 'instagram.png'},
-    {nome:'PPDM', icon: 'instagram.png'},
-    {nome:'PRO', icon: 'whatsapp.png'},
-    {nome:'PWBE', icon:'youtube.png'},
-    {nome:'PWFE', icon: 'instagram.png'}
+    {nome:'PPDM', icon: 'instagram.png', cor: 'yellow'},
+    {nome:'PRO', icon: 'whatsapp.png', cor: 'red'},
+    {nome:'PWBE', icon:'youtube.png', cor: 'purple'},
+    {nome:'PWFE', icon: 'instagram.png', cor: 'blue'}
 ]
 
 function criarMenu(disciplina) {
@@ -22,8 +18,17 @@ function criarMenu(disciplina) {
 
     novoItem.appendChild(novaImagem)
     novoItem.appendChild(novoSpan)
+    novoItem.style = `--cor-hover:${disciplina.cor}`
 
     lista.appendChild(novoItem)
 }
 
 disciplinas.forEach(criarMenu)
+
+const produtos = [
+    {nome: 'Camisa de Goleiro Corinthians 2024/25', icon: 'corinthians-laranja', cor: '#fc8542'},
+    {nome: 'Camisa de Goleiro Corinthians 2024/25 PRO', icon: 'corinthians-azul', cor: '#32b2c9'},
+    {nome: 'Camisa Nike Corinthians II 2024/25', icon: 'corinthians-preto', cor: 'black'},
+    {nome: 'Camisa Corinthians Treino 2025', icon: 'corinthians-roxa', cor: '#342059'},
+    {nome: 'Camisa Corinthians Japão', icon: 'corinthians-bege', cor: '#f9f6e1'}
+]
